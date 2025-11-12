ab3dad6e06f14f18ac06383b076e716736c4efbe
vdalseng <vdalseng@gmail.com>
2025-11-12 12:33:07 +0100

    Add example network module

---
+++ terraform-virtualnetwork-module/examples/variables.tf
@@ -0,0 +1,4 @@
+variable "system_name" {
+    description = "The canonical name of the system using this virtual network."
+    type        = string
+}
---
+++ terraform-virtualnetwork-module/terraform.tfvars
@@ -0,0 +1,7 @@
+system_name = "demo"
+environment = "dev"
+
+location = "Norway East"
+
+vnet_a_address_space = "10.133.100.0/23"
+
